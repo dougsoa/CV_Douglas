4f7e768ba77d20b03674d06a097f9be5f766b389
Douglas Oliveira <83471539+dougsoa@users.noreply.github.com>
2022-08-23 14:22:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function index() {
+  window.location = "index.html" ;
+}
+
+function experiencia() {
+  window.location = "experiencia.html" ;
+}
+
+function formacao() {
+  window.location = "formacao.html" ;
+}
+  
+function linguas() {
+  window.location = "linguas.html" ;
+}
+  
+function contato() {
+  window.location = "contato.html" ;
+}
+
+
+// BARRA DE PROGRESSO LINGUAS
+let barra=document.getElementById("pb1");
+let ingles=0
+let intBarra=setInterval(()=>{
+barra.style.width= ingles+"%";
+ingles++;
+if(ingles>=40){
+  ingles=0;
+}
+},500)
+
+let barraesp=document.getElementById("pb2");
+let espanhol=0
+let intBarraesp=setInterval(()=>{
+barraesp.style.width= espanhol+"%";
+espanhol++;
+if(espanhol>=85){
+  espanhol=0;
+}
+},500)
+   
